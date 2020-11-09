e07d2eb90fc6e3a1c77871f56faa439ab3a9649b
lijiajia <343105980@qq.com>
2020-11-09 17:49:33 +0800

    添加错误处理机制

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 
 import click
 import os
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 # 引入配置字典
 from app.configs import configs
 from app.libs.extensions import db, migrate, get_login_manager, csrf_protect, mail ,  whooshee
@@ -11,7 +11,12 @@ from app.models import Post, Category, post_category_middle, Comment, Admin, Lin
 # 引入 datetime
 from datetime import datetime
 from app.libs.custom_filters import switch_link_tag , get_search_part
-
+# 额外引入 func 通过 func.rand() 获取随机排序的文章
+from sqlalchemy import func
+# 要处理 csrf 的错误，需要专门引入 CSRFError
+from flask_wtf.csrf import CSRFError
+# 引入 is_safe_url 判断 request.referrer 的 URL 是否合法
+from app.libs.helpers import is_safe_url
 
 
 
@@ -22,19 +27,16 @@ def create_app(config="development"):
     app = Flask(__name__)
     app.config.from_object(configs[config])
     register_extensions(app)
-
+    # 调用并注册相关错误处理
+    register_error_templates(app)
     # 注册蓝图
     register_blueprints(app)
-
     # 调用函数
     register_template_context(app)
-
     # 注册自定义过滤器
     add_template_filters(app)
-
     # 调用注册命令函数
     register_cli(app)
-
     return app
 
 
@@ -95,6 +97,53 @@ def register_template_context(app):
         ]
         return {"admin": admin, "categories": categories, "links": links, "current_year": current_year,
                 "unreviewed_comment_count": unreviewed_comment_count, "admin_url_info": admin_url_info}
+
+
+def register_error_templates(app: Flask):
+    """
+    注册 HTTP 请求错误时页面显示模板
+    :param app: Flask 核心对象
+    :return: None
+    """
+    @app.errorhandler(404)
+    def not_found(e):
+        """处理 404 错误"""
+        error_info = {
+            'head_title': '找不到您要访问的页面',
+            'page_title': '找不到您要访问的页面...',
+            'description': f'抱歉，您要访问的页面不存在，您可以<a href="{url_for("web.index")}">返回首页</a>，或者查看以下内容：'
+        }
+        posts = Post.query.filter_by(published=True, trash=False).order_by(func.random()).limit(5)
+        return render_template('error/error.html', posts=posts, error_info=error_info), 404
+    @app.errorhandler(500)
+    def server_error(e):
+        """处理 500 错误"""
+        error_info = {
+            'head_title': '似乎有什么意外出现了',
+            'page_title': '似乎有什么意外出现了...',
+            'description': f'抱歉，有不可名状的错误突然出现，您可以<a href="{url_for("web.index")}">返回首页</a>，或者查看以下内容：'
+        }
+        posts = Post.query.filter_by(published=True, trash=False).order_by(func.random()).limit(5)
+        return render_template('error/error.html', posts=posts, error_info=error_info), 500
+    @app.errorhandler(CSRFError)
+    def csrf_error(e):
+        """处理 csrf_token 失效错误"""
+        # 因为 csrf 的错误普遍出现在填写表单中，所以为了引导用户回到上一页，这里有必要进行判断
+        if is_safe_url(request.referrer):
+            back_url = request.referrer
+        else:
+            back_url = url_for('web.index')
+        error_info = {
+            'head_title': '您的页面会话已过期',
+            'page_title': '您的页面会话已过期',
+            'description': f'抱歉，可能您在页面停留过久，导致会话已过期，您可以<a href="{back_url}">返回上一页</a>重新执行操作，或者查看以下内容：'
+        }
+        posts = Post.query.filter_by(published=True, trash=False).order_by(func.random()).limit(5)
+        return render_template('error/error.html', posts=posts, error_info=error_info), 500
+
+
+
+
 # 然后给 initdb 命令添加一个参数
 def register_cli(app: Flask):
     @app.cli.command()
